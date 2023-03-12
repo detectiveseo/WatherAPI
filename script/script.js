1f046791d1659b9047c2302a2c00e6f328802e8e
detectiveseo <detectiveseo1@gmail.com>
2023-03-12 16:49:13 +0600

    added all information dinamic

--- script/script.js
+++ script/script.js
@@ -1,1 +1,25 @@
 // 694ad86d37164675f0f64a08aca0234f
+
+const loadData = async(searchValue) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=694ad86d37164675f0f64a08aca0234f`
+    const res = await fetch(url);
+    const resJson = await res.json();
+    showData(resJson)
+
+}
+
+const getData = () => {
+    const seachFiled = document.getElementById('serch');
+    const seachFiledValue = seachFiled.value;
+    loadData(seachFiledValue);
+}
+
+const showData = (data) => {
+    grabAndChang("cityName", data.name)
+    console.log(data);
+}
+
+const grabAndChang = (id, text) => {
+    const actionDiv = document.getElementById(id);
+    actionDiv.innerText = text;
+}
